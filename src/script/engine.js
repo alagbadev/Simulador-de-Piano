ea01046a0e58cad3f68a2fade6f8c1a9f72b8b66
alagbadev <alagbadev@gmail.com>
2024-03-22 20:35:59 -0300

    Projeto Alagba's Piano 1.0

---
+++ src/script/engine.js
@@ -0,0 +1,39 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const volumeSlider = document.querySelector(".volume-slider input")
+const keysCheck = document.querySelector(".keys-check input");
+
+let mapedKeys = [];
+let audio = new Audio("src/tunes/a.wav");
+
+const playTune = (key) => {
+    audio.src = `src/tunes/${key}.wav`
+    audio.play();
+    console.log(mapedKeys)
+    const clickedkey = document.querySelector(`[data-key="${key}"]`)
+    clickedkey.classList.add("active")
+    setTimeout(()=>{
+        clickedkey.classList.remove("active");
+    },150);
+};
+
+pianoKeys.forEach((key)=>{
+    key.addEventListener("click", ()=> playTune(key.dataset.key));
+    mapedKeys.push(key.dataset.key);
+} )
+
+document.addEventListener("keydown", (e)=> {
+    if(mapedKeys.includes(e.key)){
+    playTune(e.key);
+    }
+});
+
+const handleVolume =(e) => {
+    audio.volume = e.target.value
+};
+
+const showhideKeys =() =>{
+    pianoKeys.forEach(key => key.classList.toggle("hide"))
+}
+
+volumeSlider.addEventListener("input", handleVolume);
+keysCheck.addEventListener("click", showhideKeys)
